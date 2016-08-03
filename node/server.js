eed52710af72b7b8306bbf7ecd87b854f6afe29c
sly <shanglingyue1992@163.com>
2016-08-04 00:11:34 +0800

    ,

--- node/server.js
+++ node/server.js
@@ -1,16 +1,27 @@
 var http = require('http');
 var url = require('url');
+var formidable = require("formidable");
+
 function start(route,handle){
 	http.createServer(function(request, response){
-		  var pathname = url.parse(request.url).pathname;
-		  console.log("Request for "+ pathname +" received.");
-	
-		  route(handle,pathname);
+		var postData ="";
+	    var pathname = url.parse(request.url).pathname;
+	    console.log("Request for "+ pathname +" received.");
 
-		  response.writeHead(200,{"Content-Type":"text/plain"});
-		  response.write("Hello World");
-		  response.end();
+	    request.setEncoding("utf8");
+
+	    request.addListener("data",function(postDataChunk){
+	      postData += postDataChunk;
+	      console.log("Received POST data chunk '"+
+	      postDataChunk +"'.");
+	    });
+
+	    request.addListener("end",function(){
+	      route(handle, pathname, response, postData);
+	    });
+
 	}).listen(8888);
+
 	console.log("node js");
 
 }
